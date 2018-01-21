e27328357bb39700e407c86cf65f417b47aaa03a
Jeff Lowery <jlowery2663@gmail.com>
2018-01-20 16:03:32 -0800

    added npm 'bare' script that talks to engine directly

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # chessQ
 Chess quality analyzer, using UCI, RabbitMQ, and GraphQL
+
+
+# Notes
+* 'option name Hash type spin default 16 min 1 max 2048'
+  * lies; 64 seems to be the max
--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 import express from 'express';
 const app = express();
+import stockfish from 'stockfish'
+const sf = stockfish();
+
+const pm = (message) => {
+  console.log("Posting:", message)
+  sf.postMessage(message)
+}
+
+sf.onmessage = function(event) {
+  console.log({event})
+};
+
+sf.postMessage("uci")
 
 app.get('/', (req, res) => {
+  // set an option
+  pm('setoption name Hash value 32')
+  pm('setoption name Contempt value 0')
+  // pm('isready')
+  pm('isready')
+  // load game
   res.send("Hello World!")
 })
 
-app.listen(3000, () => "server ready")
+app.listen(3000, () => console.log("server ready"))
