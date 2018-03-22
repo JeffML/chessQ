edc6cd394f551f0b8fc290c96f1e4bbfa876fbd6
Jeff Lowery <jeff.l@mavericklabel.com>
2018-03-21 18:39:16 -0700

    test

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
+import {
+    graphqlExpress,
+    graphiqlExpress
+} from 'apollo-server-express';
 
-import {execute, subscribe} from 'graphql'
-import {createServer} from 'http';
-import {SubscriptionServer} from 'subscriptions-transport-ws';
+import {
+    execute,
+    subscribe
+} from 'graphql'
+import {
+    createServer
+} from 'http';
+import {
+    SubscriptionServer
+} from 'subscriptions-transport-ws';
 
 import schema from './schema';
 import defaultQueries from './defaultQueries';
@@ -14,24 +24,26 @@ const app = express();
 
 const ws = createServer(app);
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}))
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+    schema
+}))
 
 ws.listen(PORT, () => {
-  console.log(`chessQ Server is now running on http://localhost:${PORT}`);
-  // Set up the WebSocket for handling GraphQL subscriptions
-  new SubscriptionServer({
-    execute,
-    subscribe,
-    schema
-  }, {
-    server: ws,
-    path: '/subscriptions'
-  });
+    console.log(`The chessQ Server is now running on http://localhost:${PORT}`);
+    // Set up the WebSocket for handling GraphQL subscriptions
+    new SubscriptionServer({
+        execute,
+        subscribe,
+        schema
+    }, {
+        server: ws,
+        path: '/subscriptions'
+    });
 });
 
 const opts = {
-  endpointURL: '/graphql',
-  // query: defaultQueries,
-  subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
+    endpointURL: '/graphql',
+    // query: defaultQueries,
+    subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
 };
 app.get('/graphiql', graphiqlExpress(opts));
