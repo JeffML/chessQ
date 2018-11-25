be7545f01b233638d6ccff1788e98b24d58eca3f
Jeff Lowery <jlowery2663@gmail.com>
2018-11-24 17:04:55 -0800

    refactor fetch

--- test-api/UCI_test.js
+++ test-api/UCI_test.js
@@ -1,24 +1,8 @@
 /* eslint-env mocha */
-import { createApolloFetch } from 'apollo-fetch';
 import chai from 'chai';
+import fetch from './fetch';
 
 chai.should();
-
-const uri = 'http://localhost:3001/graphql';
-const apolloFetch = createApolloFetch({ uri });
-
-const fetch = query => new Promise((resolve, reject) => {
-  apolloFetch({ query /* variables, operationName */ }) // all apolloFetch arguments are optional
-    .then((result) => {
-      const { data, errors /* , extensions */ } = result;
-      if (errors) {
-        console.error(errors);
-        reject(new Error(errors));
-      } else {
-        resolve(data);
-      }
-    });
-});
 
 describe('UCI request', function () {
   this.timeout(5000);
